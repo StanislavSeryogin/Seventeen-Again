743a54286557b9e9737e78d46ed718fd08386e34
Stanislav <stas.seryogin.87@gmail.com>
2023-09-07 10:03:00 +0300

    Initial Commit

---
+++ Seventeen Again/Seventeen_AgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  Seventeen_AgainApp.swift
+//  Seventeen Again
+//
+//  Created by Stanislav Seryogin on 07.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Seventeen_AgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
